86dfb892e1eb5a9d8ed6308283f7cf398ca88702
crzapatad <cr.zapatad@duocuc.cl>
2023-12-11 11:31:50 -0300

    listo

---
+++ src/app/pages/main/home/home.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
+import { AddUpdateProductComponent } from 'src/app/shared/components/add-update-product/add-update-product.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  firebaseSvc = inject(FirebaseService);
+  utilsSvc = inject(UtilsService);
+
+  ngOnInit() {
+  }
+
+  signOut() {
+    this.firebaseSvc.signOut();
+  }
+
+  addUpdateProduct(){
+
+    this.utilsSvc.presentModal({
+      component: AddUpdateProductComponent,
+      cssClass: 'add-update-modal'
+    })
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDBfIhyV3ZTTFlZuFm_tEN-kON774H49HA",
+    authDomain: "tellevoappdici.firebaseapp.com",
+    projectId: "tellevoappdici",
+    storageBucket: "tellevoappdici.appspot.com",
+    messagingSenderId: "356157679831",
+    appId: "1:356157679831:web:610aadb02002d6b1e62892"
+  }
+};
